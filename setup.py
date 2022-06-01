d3ae24fff8e7462888f8e267c4acaea6269cb49d
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-06-01 16:47:01 +0100

    Update setup info

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ setup(
     author='Adam Richard-Bollans',
     author_email='38588335+alrichardbollans@users.noreply.github.com',
     # Needed to actually package something
-    packages=find_packages(include=['cleaning_plant_occurrences', 'climate_vars', 'common_name_vars',
+    packages=find_packages(include=['cleaning_logan_plant_occurrences', 'logan_climate_vars', 'logan_common_name_vars',
                                     'conservation_priorities',
-                                    'getting_malarial_regions',
+                                    'getting_logan_malarial_regions',
                                     'logan_poison_vars',
                                     'manually_collected_data', 'morphological_vars',
-                                    'metabolite_vars', 'medicinal_usage_vars',
-                                    'wcsp_distributions',
+                                    'logan_metabolite_vars', 'medicinal_usage_vars',
+                                    'logan_wcsp_distributions',
                                     'logan_wikipedia_vars']),
     # *strongly* suggested for sharing
     version='0.1',
