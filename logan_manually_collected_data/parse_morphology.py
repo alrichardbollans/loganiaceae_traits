ed8a8e2ac921c8b4ffcba21d8e84d02ddb9e9727
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-06-15 14:52:30 +0100

    Add prelim manual data

--- logan_manually_collected_data/parse_morphology.py
+++ logan_manually_collected_data/parse_morphology.py
@@ -4,12 +4,12 @@ import pandas as pd
 from automatchnames import get_accepted_info_from_names_in_column
 from pkg_resources import resource_filename
 
-from logan_manually_collected_data import trait_parsing_output_path
+from logan_manually_collected_data import logan_trait_parsing_output_path
 
 _inputs_path = resource_filename(__name__, 'inputs')
 _manual_habit_data_csv = os.path.join(_inputs_path, 'manual_habits.csv')
 
-manual_habit_data_output = os.path.join(trait_parsing_output_path, 'manual_habit_data.csv')
+manual_habit_data_output = os.path.join(logan_trait_parsing_output_path, 'manual_habit_data.csv')
 
 
 def prepare_habits():
