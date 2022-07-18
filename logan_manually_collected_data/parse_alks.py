dfa2cc1a1038df9e28302425d60d070bc2a02dbc
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-07-18 13:53:21 +0100

    Update manual trait file

--- logan_manually_collected_data/parse_alks.py
+++ logan_manually_collected_data/parse_alks.py
@@ -7,7 +7,8 @@ from logan_manually_collected_data import logan_trait_parsing_output_path, logan
 logan_alk_hits_manual_output_csv = os.path.join(logan_trait_parsing_output_path, 'logan_alks_manual.csv')
 
 def get_manual_alk_hits():
-    manual_df = pd.read_csv(logan_encoded_traits_csv)
+    manual_df = pd.read_csv(logan_encoded_traits_csv,index_col=0
+                            )
     # Get steroids
     alk_hits = manual_df[manual_df['Alkaloids'] == 1]
 
