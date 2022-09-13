4a4815e21e46b551f81497e6fa6881992745d77b
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2022-09-13 15:00:44 +0100

    Update tests

--- logan_unittests_for_vars/test_importing_trait_data.py
+++ logan_unittests_for_vars/test_importing_trait_data.py
@@ -23,6 +23,8 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(trait_df['Family'].unique().tolist(), ['Loganiaceae'])
 
+        self.assertEqual(len(trait_df[trait_df['Accepted_Rank']=='Genus']), 0)
+
     def test_genera_from_acc_names(self):
         self.assertEqual(get_genus_from_full_name('Danais'), 'Danais')
         self.assertEqual(get_genus_from_full_name('Danais spp.'), 'Danais')
